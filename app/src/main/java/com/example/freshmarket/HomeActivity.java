52f9039b10c37c72d8a9bc94800d171e8a933508
quangvu0905 <97514754+Thombui@users.noreply.github.com>
2022-04-25 14:01:52 +0700

    Login FireBase + ProgressBar

--- app/src/main/java/com/example/freshmarket/HomeActivity.java
+++ app/src/main/java/com/example/freshmarket/HomeActivity.java
@@ -5,13 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
 
+    ProgressBar progressBar;
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        auth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressbar2);
+        progressBar.setVisibility(View.GONE);
+
+        if(auth.getCurrentUser() != null){
+            progressBar.setVisibility(View.VISIBLE);
+            startActivity(new Intent(HomeActivity.this, MainActivity.class));
+            Toast.makeText(this,"please wait you are already logged in",Toast.LENGTH_SHORT).show();
+            finish();
+        }
     }
 
     public void login(View view) {
--- app/src/main/java/com/example/freshmarket/RegistrationActivity.java
+++ app/src/main/java/com/example/freshmarket/RegistrationActivity.java
@@ -9,13 +9,16 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.freshmarket.models.UserModel;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -23,6 +26,8 @@ public class RegistrationActivity extends AppCompatActivity {
     EditText name, email, password;
     TextView signIn;
     FirebaseAuth auth;
+    FirebaseDatabase database;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +35,11 @@ public class RegistrationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_registration);
 
         auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        progressBar = findViewById(R.id.progressbar);
+        progressBar.setVisibility(View.GONE);
+
         signUp = findViewById(R.id.reg_btn);
         name = findViewById(R.id.name);
         email = findViewById(R.id.email_reg);
@@ -47,6 +57,7 @@ public class RegistrationActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 createUser();
+                progressBar.setVisibility(View.VISIBLE);
             }
         });
     }
@@ -82,9 +93,14 @@ public class RegistrationActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
+                            UserModel userModel = new UserModel(userName,userEmail,userPassword);
+                            String id = task.getResult().getUser().getUid();
+                            database.getReference().child("Users").child(id).setValue(userModel);
+                            progressBar.setVisibility(View.GONE);
                             Toast.makeText(RegistrationActivity.this ,"Registration Successful!",Toast.LENGTH_SHORT).show();
                         }
                         else {
+                            progressBar.setVisibility(View.GONE);
                             Toast.makeText(RegistrationActivity.this ,"Error" + task.getException(),Toast.LENGTH_SHORT).show();
                         }
                     }
