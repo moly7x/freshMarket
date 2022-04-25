96f8f0fcc5f5081edb49c01e89e638febd216fb5
quangvu0905 <77672018+htrang1815@users.noreply.github.com>
2022-04-25 11:56:02 +0700

    Login+RegisterActivity

---
+++ app/src/main/java/com/example/freshmarket/RegistrationActivity.java
@@ -0,0 +1,36 @@
+package com.example.freshmarket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    Button signUp;
+    EditText name, email, password;
+    TextView signIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        signUp = findViewById(R.id.reg_btn);
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email_reg);
+        password = findViewById(R.id.password_reg);
+        signIn = findViewById(R.id.sign_in_1);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
+            }
+        });
+    }
+}
