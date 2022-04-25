4bda7c852b541a76e41a98bcd96a9812eb2737a9
quangvu0905 <16718453+moly7x@users.noreply.github.com>
2022-04-25 20:48:16 +0700

    Update

--- app/src/main/java/com/example/freshmarket/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/freshmarket/ui/home/HomeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -34,6 +36,9 @@ public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
 
+    ScrollView scrollView;
+    ProgressBar progressBar;
+
     RecyclerView popularRec, homeCatRec,recommendedRec;
     FirebaseFirestore db;
 
@@ -58,6 +63,11 @@ public class HomeFragment extends Fragment {
         popularRec = root.findViewById(R.id.pop_rec);
         homeCatRec = root.findViewById((R.id.explore_rec));
         recommendedRec = root.findViewById(R.id.recommended_rec);
+        scrollView = root.findViewById(R.id.scroll_view);
+        progressBar = root.findViewById(R.id.progressbar5);
+
+        progressBar.setVisibility(View.VISIBLE);
+        scrollView.setVisibility(View.GONE);
 
         //Popular items
         popularRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
@@ -74,6 +84,9 @@ public class HomeFragment extends Fragment {
                                 PopularModel popularModel = document.toObject(PopularModel.class);
                                 popularModelList.add(popularModel);
                                 popularAdapters.notifyDataSetChanged();
+
+                                progressBar.setVisibility(View.GONE);
+                                scrollView.setVisibility(View.VISIBLE);
 
                             }
                         } else {
