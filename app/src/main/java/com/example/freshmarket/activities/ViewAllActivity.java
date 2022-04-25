8a05e16bd83c3821b3cdc8f21c1e67819e6be1d0
quangvu0905 <86471650+Duonghuong01@users.noreply.github.com>
2022-04-25 23:59:29 +0700

    ViewAllActivity

---
+++ app/src/main/java/com/example/freshmarket/activities/ViewAllActivity.java
@@ -0,0 +1,42 @@
+package com.example.freshmarket.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.freshmarket.R;
+import com.example.freshmarket.adapters.ViewAllAdapter;
+import com.example.freshmarket.models.ViewAllModel;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewAllActivity extends AppCompatActivity {
+
+    FirebaseFirestore firestore;
+    RecyclerView recyclerView;
+    ViewAllAdapter viewAllAdapter;
+    List<ViewAllModel> viewAllModelList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_all);
+
+        firestore = FirebaseFirestore.getInstance();
+
+        String type = getIntent().getStringExtra("type");
+        recyclerView = findViewById(R.id.view_all_rec);
+        recyclerView.setVisibility(View.GONE);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        viewAllModelList = new ArrayList<>();
+        viewAllAdapter = new ViewAllAdapter(this, viewAllModelList);
+        recyclerView.setAdapter(viewAllAdapter);
+
+    }
+}
--- app/src/main/java/com/example/freshmarket/adapters/PopularAdapters.java
+++ app/src/main/java/com/example/freshmarket/adapters/PopularAdapters.java
@@ -1,6 +1,7 @@
 package com.example.freshmarket.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.freshmarket.R;
+import com.example.freshmarket.activities.ViewAllActivity;
 import com.example.freshmarket.models.PopularModel;
 
 import java.util.List;
@@ -39,6 +41,15 @@ public class PopularAdapters extends RecyclerView.Adapter<PopularAdapters.ViewHo
         holder.rating.setText(popularModelList.get(position).getRating());
         holder.description.setText(popularModelList.get(position).getDescription());
         holder.discount.setText(popularModelList.get(position).getDiscount());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, ViewAllActivity.class);
+                intent.putExtra("type",popularModelList.get(position).getType());
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
