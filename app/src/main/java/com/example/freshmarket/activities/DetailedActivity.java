1ee2549be46c6f2d989fc8881d0f6a8fdf9b1c0c
quangvu0905 <91740402+quangbeuu@users.noreply.github.com>
2022-04-26 00:47:26 +0700

    DetailedActivity

---
+++ app/src/main/java/com/example/freshmarket/activities/DetailedActivity.java
@@ -0,0 +1,16 @@
+package com.example.freshmarket.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.freshmarket.R;
+
+public class DetailedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed);
+    }
+}
