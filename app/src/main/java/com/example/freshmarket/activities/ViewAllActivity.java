4be6b6ab0d49793fe376eb974ebf02aa274114fb
quangvu0905 <86471650+Duonghuong01@users.noreply.github.com>
2022-04-26 00:09:06 +0700

    Get fruits + vegetable

--- app/src/main/java/com/example/freshmarket/activities/ViewAllActivity.java
+++ app/src/main/java/com/example/freshmarket/activities/ViewAllActivity.java
@@ -1,5 +1,6 @@
 package com.example.freshmarket.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,7 +11,11 @@ import android.view.View;
 import com.example.freshmarket.R;
 import com.example.freshmarket.adapters.ViewAllAdapter;
 import com.example.freshmarket.models.ViewAllModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,12 +36,43 @@ public class ViewAllActivity extends AppCompatActivity {
 
         String type = getIntent().getStringExtra("type");
         recyclerView = findViewById(R.id.view_all_rec);
-        recyclerView.setVisibility(View.GONE);
+
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         viewAllModelList = new ArrayList<>();
         viewAllAdapter = new ViewAllAdapter(this, viewAllModelList);
         recyclerView.setAdapter(viewAllAdapter);
+
+        //////////Geting Fruits
+        if (type != null && type.equalsIgnoreCase("fruit")) {
+            firestore.collection("AllProducts").whereEqualTo("type", "fruit").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                    for (DocumentSnapshot documentSnapshot : task.getResult().getDocuments()) {
+                        ViewAllModel viewAllModel = documentSnapshot.toObject(ViewAllModel.class);
+                        viewAllModelList.add(viewAllModel);
+                        viewAllAdapter.notifyDataSetChanged();
+                    }
+                }
+            });
+        }
+
+        //////////Geting vegetable
+        if (type != null && type.equalsIgnoreCase("vegetable")) {
+            firestore.collection("AllProducts").whereEqualTo("type", "vegetable").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                    for (DocumentSnapshot documentSnapshot : task.getResult().getDocuments()) {
+                        ViewAllModel viewAllModel = documentSnapshot.toObject(ViewAllModel.class);
+                        viewAllModelList.add(viewAllModel);
+                        viewAllAdapter.notifyDataSetChanged();
+
+                    }
+                }
+            });
+        }
 
     }
 }
