cacd2863cd78c74c3145072e0a889f7b210778f2
quangvu0905 <86471650+Duonghuong01@users.noreply.github.com>
2022-04-25 23:24:34 +0700

    Cart + Product + Order

--- app/src/main/java/com/example/freshmarket/ui/category/CategoryFragment.java
+++ app/src/main/java/com/example/freshmarket/ui/category/CategoryFragment.java
@@ -4,15 +4,38 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.freshmarket.R;
+import com.example.freshmarket.adapters.NavCategoryAdapter;
 import com.example.freshmarket.databinding.FragmentCategoryBinding;
+import com.example.freshmarket.models.NavCategoryModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CategoryFragment extends Fragment {
+
+    FirebaseFirestore db;
+    RecyclerView recyclerView;
+
+    List<NavCategoryModel> categoryModelList;
+    NavCategoryAdapter navCategoryAdapter;
+
+    ProgressBar progressBar;
 
     private FragmentCategoryBinding binding;
 
@@ -22,6 +45,36 @@ public class CategoryFragment extends Fragment {
         binding = FragmentCategoryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        db = FirebaseFirestore.getInstance();
+        recyclerView = root.findViewById(R.id.cat_rec);
+        progressBar = root.findViewById(R.id.progressbar6);
+        progressBar.setVisibility(View.VISIBLE);
+        recyclerView.setVisibility(View.GONE);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.VERTICAL,false));
+        categoryModelList = new ArrayList<>();
+        navCategoryAdapter = new NavCategoryAdapter(getActivity(),categoryModelList);
+        recyclerView.setAdapter(navCategoryAdapter);
+        db.collection("NavCategory")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                NavCategoryModel navCategoryModel = document.toObject(NavCategoryModel.class);
+                                categoryModelList.add(navCategoryModel);
+                                navCategoryAdapter.notifyDataSetChanged();
+                                progressBar.setVisibility(View.GONE);
+                                recyclerView.setVisibility(View.VISIBLE);
+                            }
+                        } else {
+                            Toast.makeText(getActivity(),"Error"+task.getException(),Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                            recyclerView.setVisibility(View.VISIBLE);
+                        }
+                    }
+                });
         return root;
     }
 
